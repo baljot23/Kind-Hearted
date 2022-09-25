b656167c91be1c67a7b93a633bd36debe1fe1f76
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-09-25 16:39:16 -0400

    Kind-Hearted

--- client/src/App.js
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className="">hello</div>;
-};
-
-export default App;
---
+++ client/src/components/Announcement.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Announcement = () => {
+  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
+};
+
+export default Announcement;
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+`;
