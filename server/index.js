cb3be2db1176d3df66e10907a6a83de0db8c42d1
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-09-29 20:24:23 -0400

    server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,26 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://Baljot:<password>@kind-hearted.abcf9rw.mongodb.net/shop?retryWrites=true&w=majority"
-);
+const dotenv = require("dotenv");
 
-app.listen(5000, () => {
+const userRoute = require("./routes/user");
+
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
