a8309688893a9c6bc391452b869bdab2a083f08d
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-09-30 21:38:30 -0400

    server

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const userRoute = require("./routes/user");
 
 const authRoute = require("./routes/auth");
 
+const productRoute = require("./routes/product");
+
+const cartRoute = require("./routes/cart");
+
+const orderRoute = require("./routes/order");
+
 dotenv.config();
 
 mongoose
@@ -23,6 +29,9 @@ app.use(express.json());
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
